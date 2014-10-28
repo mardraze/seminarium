cb71944894928492ad9e673b040dd3fc4b0e4891
Marcin Drazek <marcindr1@gmail.com>
2014-10-28 21:43:07 +0100

    .

---
+++ commit.bat
@@ -0,0 +1,4 @@
+
+git add bower.json README.md package.json  Gruntfile.js commit.bat config.xml deploy.bat app test docs
+git commit -m "."
+git push
---
+++ deploy.bat
@@ -0,0 +1,2 @@
+call grunt %1 
+call adb install -r platforms\android\ant-build\Seminarium-debug-unaligned.apk 
